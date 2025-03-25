e192e93c983c110235ac61503f30fe09af6c523a
aflood-32 <46991740+aflood-32@users.noreply.github.com>
2025-03-25 15:17:30 +0200

    RMTD-0001::commit hooks setup, path aliases, css reset

--- src/app/app/App.test.tsx
+++ src/app/app/App.test.tsx
@@ -2,10 +2,6 @@ import { render } from "@testing-library/react";
 
 import App from "./index.tsx";
 
-test("demo", () => {
-  expect(true).toBe(true);
-});
-
 test("Renders the main page", () => {
   render(<App />);
   expect(true).toBeTruthy();
